72a1e7e7735261b1b61b76e0fd914ff4e36cda19
DifexRV <roOmkin@gmail.com>
2022-10-09 16:34:48 +0700

    Ветка для ДЗ 3.3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    System.out.println("Задача 1");
+    int deposit = 29000;
+    int total = 0;
+        for (int i = 0; i < 12; i++) {
+            total = total + deposit;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+
+    System.out.println("Задача 2");
+    int deposit2 = 29000;
+    int total2 = 0;
+        for (int i = 0; i < 12; i++) {
+            total2 = total2 + total2/100;
+            total2 = total2 + deposit2;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total2 + " рублей");
+        }
+
     }
 }
